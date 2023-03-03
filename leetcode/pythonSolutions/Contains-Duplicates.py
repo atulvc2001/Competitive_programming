9e732c8a2d8d87f72b3405c49cc0cd96d3e5525c
atvc0202 <atulvc2001@gmail.com>
2023-03-03 23:54:38 +0530

    reformatted code

--- leetcode/pythonSolutions/Contains-Duplicates.py
+++ leetcode/pythonSolutions/Contains-Duplicates.py
@@ -1,7 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
-        
+        print("Hashset is the set")
         for n in nums:
             if n in hashset:
                 return True
