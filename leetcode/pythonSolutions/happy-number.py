f5de9dacccbd8875ea33011dd596eb2fb8b45377
Atul V Chandran <atulvc2001@gmail.com>
2024-01-30 11:37:05 +0530

    update: python solution of happy number

--- leetcode/pythonSolutions/happy-number.py
+++ leetcode/pythonSolutions/happy-number.py
@@ -19,3 +19,41 @@ class Solution:
                 n = n//10
 
         return False
+
+'''
+    class Solution:
+        def isHappy(self, n: int) -> bool:
+            visit = set()
+
+            while n not in visit:
+                visit.add(n)
+                n = self.sumOfSquares(n)
+
+                if n == 1:
+                    return True
+
+            return False
+
+        def sumOfSquares(self, n: int) -> int:
+            output = 0
+            
+            while n: 
+                digit = n % 10
+                digit = digit ** 2
+                output += digit
+                n = n//10
+            return output
+
+
+ # the above, better structured answer is from neetcode 
+
+
+
+
+
+
+
+
+
+
+'''
