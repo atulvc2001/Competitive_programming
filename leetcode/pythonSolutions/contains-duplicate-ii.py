e247fe3a8378978aa5d752875fce02d770660fee
Atul V Chandran <atulvc2001@gmail.com>
2024-01-31 14:46:11 +0530

    update: contains duplicate ii

--- leetcode/pythonSolutions/contains-duplicate-ii.py
+++ leetcode/pythonSolutions/contains-duplicate-ii.py
@@ -22,3 +22,24 @@ class Solution:
         return False
 
         
+
+"""
+
+Sharing the neetcode solution as it is better structured and more efficient
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        window = set()
+        L = 0
+
+        for R in range(len(nums)):
+            if R - L > k : 
+                window.remove(nums[L])
+                L+=1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False
+
+"""
