4149c01100f046392cebdf771d516a2aabdf17cf
Atul V Chandran <atulvc2001@gmail.com>
2024-03-06 13:10:25 +0530

    feat: added an alternative solution for basic calculator ii

--- leetcode/pythonSolutions/basic-calculator-ii.py
+++ leetcode/pythonSolutions/basic-calculator-ii.py
@@ -17,3 +17,55 @@ class Solution:
                 PreSign=c
                 num=0             
         return sum(stack)
+
+
+"""
+the below solution uses O(N) T.O and O(1) S.O
+    
+class Solution:
+    def calculate(self, s: str) -> int:
+        
+
+        i = 0
+         
+        cur = prev = res = 0
+        cur_operation = "+"
+
+        while i < len(s):
+            cur_char = s[i]
+
+            if cur_char.isdigit():
+                while i < len(s) and s[i].isdigit():
+                    cur = cur * 10 + int(s[i])
+
+                    i+= 1
+                
+                i-= 1
+
+                if cur_operation == "+":
+                    res += cur
+                    prev = cur
+
+                elif cur_operation == "-":
+                    res -= cur
+                    prev = -cur
+                elif cur_operation == "*":
+                    res -= prev
+                    res += prev*cur
+                    prev = cur * prev
+                else: 
+                    res -= prev
+                    res += int(prev/cur)
+
+                    prev = int(prev/cur)
+            
+                cur = 0
+            elif cur_char != " ":
+                cur_operation = cur_char
+            
+            i+=1
+
+        return res
+
+
+"""
